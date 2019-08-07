b3b7ff5cc55ceda0fd31bad4eb5ed5afeb3eddf7
Hannah Matthewson <hannahmatthewson2@gmail.com>
2019-08-07 16:30:33 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,5 +2,12 @@ def fizzbuzz (number)
   if (number % 3 == 0 )
     "Fizz"
   end
-  
+  if (number % 5 == 0)
+    "Buzz"
+  end
+  if (number % 3 and number % 5 == 0)
+    "FizzBuzz"
+  end
+  if (number % 3 and number % 5 != 0)
+    "nil"
 end
